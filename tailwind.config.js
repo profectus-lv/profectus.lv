9e6ddfa3b9f14fd9b50ddc2d4aa73e0408a02acb
Dmitry Golubev <lastguru@gmail.com>
2025-01-06 04:54:30 +0200

    svg and style cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    darkMode: "class",
     content: [
         "./content/**/*.{js,njk}",
         "./src/*.js"
@@ -20,39 +19,17 @@ module.exports = {
             addUtilities(extendUnderline);
         }
     ],
-    variants: {
-        extend: {
-            backgroundImage: ["dark"],
-            fill: ["dark"],
-            fontWeight: ["dark"],
-            gradientColorStops: ["dark"],
-            stroke: ["dark"]
-        }
-    },
     theme: {
         fontFamily: {
             'body': ['Montserrat', 'Verdana', 'Geneva', 'ui-sans-serif', 'sans-serif', 'system-ui'],
             'header': ['"Titillium Web"', 'Verdana', 'Geneva', 'ui-sans-serif', 'sans-serif', 'system-ui']
         },
         extend: {
-            backgroundColor: (theme) => ({
-                ...theme("colors"),
-                "dark-nav": "#242424",
-                "dark-body": "#1B1B1E",
-                "dark-heading": "#27282B"
+            backgroundColor: () => ({
+                "sidebar-dark": "#222222",
             }),
-            backgroundImage: () => ({
-                "sidebar-dark":
-                    "radial-gradient(circle, #242424 0%, #1d1f27 100%)"
-            }),
-            borderWidth: (theme) => ({
-                ...theme("width"),
+            borderWidth: () => ({
                 1: "1px"
-            }),
-            gradientColorStops: (theme) => ({
-                ...theme("colors"),
-                "dark-outer": "#1B1B1E",
-                "dark-middle": "#242424"
             }),
             gridTemplateColumns: {
                 small: "0 auto",
@@ -62,9 +39,6 @@ module.exports = {
             lineHeight: {
                 pagination: "1.8rem",
                 12: "3rem"
-            },
-            margin: {
-                15: "3.75rem"
             },
             maxWidth: {
                 content: "80rem"
