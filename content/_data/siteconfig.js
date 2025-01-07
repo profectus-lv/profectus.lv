4167d0e5aa8922b3fa057070a94f2503b63d9edf
Dmitry Golubev <lastguru@gmail.com>
2025-01-07 23:50:21 +0200

    lighthouse fixes, other css fixes

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -11,11 +11,18 @@ module.exports = {
     author: "Domnīca Profectus",
     // Site description, shown below site image (optional)
     description: "Stiprinām demokrātiju un pilsonisko sabiedrību Latvijā un veicinām tās ilgtspēju",
-    // OpenGraph default image, in case you don't have an `image`
-    // set in your Markdown frontmatter; relevant for social
-    // sharing.
+    // Locale is used for OpenGraph and HTML lang property
     locale: "lv_LV",
+    // Defines fonts to load from Google Fonts
+    // Two fonts are used in this template: header and body
+    // Weights need to be defined as well
+    // Weights starting with "0," are non-italic
+    // Weights starting with "1," are italic
+    // Value of "swap" controls "font-display: swap" behavior:
+    // - disabled by default to avoid flash unstyled text (FOUT)
+    // - if flash of invisible text (FOIT) is more disturbing, enable it
     fonts: {
+        swap: false,
         body: {
             family: "Montserrat",
             weights: "0,400;0,700;1,400;1,700"
@@ -25,7 +32,11 @@ module.exports = {
             weights: "0,400;0,500;0,600;0,700"
         }
     },
+    // OpenGraph default image, in case you don't have an `image`
+    // set in your Markdown frontmatter; relevant for social
+    // sharing.
     openGraphDefaultImage: "/images/profectus-logo-wt.png",
+    // Collection of Favicons, Apple Touch icons and Webmanifest icons
     favicons: {
         "16x16": "/assets/favicons/profectus-favicon-16.png",
         "32x32": "/assets/favicons/profectus-favicon-32.png",
