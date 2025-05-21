188cdff91fdb6aff3ccc870334756d7d21545ae9
Dmitry Golubev <lastguru@gmail.com>
2025-05-21 21:08:10 +0300

    localize share tooltips

--- content/_data/sitestrings.js
+++ content/_data/sitestrings.js
@@ -1,6 +1,8 @@
 export default {
     words: "vārdi",
     share: "Dalīties",
+    share_on: "Dalīties tīklā",
+    copy_to_clipboard: "Kopēt saiti starpliktuvē",
     events: "Notikumi",
     older: "Vecāks",
     newer: "Jaunāks"
