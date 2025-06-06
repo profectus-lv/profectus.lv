787ac16b5d194ee33b1bc337a32929a738a55ead
Dmitry Golubev <lastguru@gmail.com>
2025-06-06 17:20:15 +0300

    translation fix, css height fixes

--- content/_data/sitestrings.js
+++ content/_data/sitestrings.js
@@ -3,6 +3,7 @@ export default {
     share: "Dalīties",
     share_on: "Dalīties tīklā",
     copy_to_clipboard: "Kopēt saiti starpliktuvē",
+    copied: "Saite ir nokopēta",
     events: "Notikumi",
     older: "Vecāks",
     newer: "Jaunāks"
